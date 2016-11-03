955939cea064ee82a09ce0e29bfdde75ee893a29
Andrés Vieira <anvieiravazquez@gmail.com>
2016-11-03 11:34:35 +0100

    Build scripts changed as they were copying files to the wrong directory

--- build-release.bat
+++ build-release.bat
@@ -4,11 +4,10 @@ mkdir release-windows
 cd release-windows
 pyinstaller --onefile --windowed --icon=..\installer\resource\install.ico ..\installer\installer.py
 xcopy .\dist\installer.exe .\ /Y
-xcopy ..\DSPH.py .\ /Y
-xcopy ..\LICENSE .\ /Y
-xcopy ..\DSPH_Images .\DSPH_Images\ /S /E /Y
 xcopy ..\installer\resource .\resource\ /S /E /Y
+xcopy ..\DSPH.py .\resource /Y
+xcopy ..\LICENSE .\resource /Y
+xcopy ..\DSPH_Images .\resource\DSPH_Images\ /S /E /Y
 del .\installer.spec /Q
 rmdir .\build /S /Q
 rmdir .\dist /S /Q
-pause
--- build-release.sh
+++ build-release.sh
@@ -4,10 +4,10 @@ mkdir release-linux
 cd release-linux
 pyinstaller --onefile --windowed --icon=../installer/resource/install.ico ../installer/installer.py
 cp ./dist/installer ./
-cp ../DSPH.py ./
-cp ../LICENSE ./
-cp -rf ../DSPH_Images/ ./
 cp -rf ../installer/resource ./
+cp ../DSPH.py ./resource/
+cp ../LICENSE ./resource/
+cp -rf ../DSPH_Images/ ./resource/
 rm ./installer.spec
 rm -rf ./build
 rm -rf ./dist
