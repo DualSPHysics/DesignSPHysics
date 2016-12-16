d8adad05fc5cdbb48300146afdbdb4f55c48908c
Andrés Vieira <anvieiravazquez@gmail.com>
2016-12-16 11:31:02 +0100

    Included test examples.
    Former-commit-id: bac40be98836b7c0a746833d36f0d9395179416a

---
+++ test-examples/initials_example/run.bat
@@ -0,0 +1,9 @@
+@echo off
+echo "------- Autoexported by DesignSPHysics -------"
+echo "This script executes GenCase for the case saved, that generates output files in the *_Out dir. Then, executes a simulation on CPU of the case. Last, it exports all the geometry generated in VTK files for viewing with ParaView."
+pause
+"C:/DualSPHysics/EXECS/GenCase4_win64.exe" C:/Users/ndrs/Desktop/initials_example/initials_example_Def C:/Users/ndrs/Desktop/initials_example/initials_example_Out/initials_example -save:+all
+"C:/DualSPHysics/EXECS/DualSPHysics4_win64.exe" C:/Users/ndrs/Desktop/initials_example/initials_example_Out/initials_example C:/Users/ndrs/Desktop/initials_example/initials_example_Out -svres -cpu
+"C:/DualSPHysics/EXECS/PartVTK4_win64.exe" -dirin C:/Users/ndrs/Desktop/initials_example/initials_example_Out -savevtk C:/Users/ndrs/Desktop/initials_example/initials_example_Out/PartAll
+echo "------- Execution complete. If results were not the exepected ones check for errors. Make sure your case has a correct DP specification. -------"
+pause
