489c54bc8ded812a487ad12b7445fe89e88dfc8a
Andrés Vieira <anvieiravazquez@gmail.com>
2019-11-21 13:05:50 +0100

    Moorings | XML: Adds Moorings/MoorDyn Renderer
    Former-commit-id: e3183bf071b305d8ab870712de5ca47fcdde3a8b

--- mod/dataobjects/moorings/moordyn/moordyn_output_configuration.py
+++ mod/dataobjects/moorings/moordyn/moordyn_output_configuration.py
@@ -8,3 +8,10 @@ class MoorDynOutputConfiguration():
 
     def __init__(self):
         super().__init__()
+        self.startTime: float = 0
+        self.endTime: float = 10
+        self.dtOut: float = 0.01
+        self.tension: str = "all"
+        self.force: str = "all"
+        self.velocity: str = "all"
+        self.position: str = "all"
