f509a2f4c72b729c5b187a3b5f4d128013b48d43
Andres Vieira <anvieiravazquez@gmail.com>
2020-03-30 10:10:16 +0200

    ApplicationSettings: Changes pickle for JSON serialization
    Pickling this object just seemed a bit overkill for what this class
    is used for. Now it serializes to JSON and loads it checking which
    properties does exist and which do not, so in the future we don't
    cause any incompatibilities.
    
    It also saves a "json_version" key which will be useful in the future
    if for some reason we want to break compatibility and need to check
    in some way if the JSON is from an old version or not

--- mod/dataobjects/application_settings.py
+++ mod/dataobjects/application_settings.py
@@ -2,12 +2,12 @@
 # -*- self.get_save_file: utf-8 -*-
 """ DesignSPHysics application settings. """
 
-import pickle
 import os
+import json
 
 import FreeCAD
 
-from mod.constants import PICKLE_PROTOCOL, VERSION
+from mod.constants import VERSION
 
 
 class ApplicationSettings():
@@ -19,6 +19,8 @@ class ApplicationSettings():
         if ApplicationSettings.__instance is not None:
             raise Exception("ApplicationSettings class is a singleton and should not be initialized twice")
         ApplicationSettings.__instance = self
+        self.json_version: int = 1
+        self.app_version: str = VERSION
         self.debug_enabled: bool = False
         self.verbose_enabled: bool = False
         self.notify_on_outdated_version_enabled: bool = True
@@ -34,21 +36,25 @@ class ApplicationSettings():
 
     def get_save_file(self) -> str:
         """ Returns the path of the configuration file saved in the FreeCAD user directory. """
-        return "{datadir}/designsphysics-settings-{version}.data".format(datadir=FreeCAD.getUserAppDataDir(), version=VERSION)
+        return "{datadir}/designsphysics-settings.json".format(datadir=FreeCAD.getUserAppDataDir())
 
     def restore_from_disk(self) -> None:
         """ Tries to restore the saved paths from the persisted ones if they exist."""
         config_file: str = self.get_save_file()
         if not os.path.exists(config_file):
             return
-        with open(config_file, "rb") as picklefile:
-            obj: ApplicationSettings = pickle.load(picklefile)
-            self.debug_enabled = obj.debug_enabled
-            self.verbose_enabled = obj.verbose_enabled
-            self.notify_on_outdated_version_enabled = obj.notify_on_outdated_version_enabled
-            self.force_moordyn_support_enabled = obj.force_moordyn_support_enabled
+        with open(config_file, "r") as save_file:
+            disk_data: dict = json.load(save_file)
+            if "debug_enabled" in disk_data.keys():
+                self.debug_enabled = disk_data["debug_enabled"]
+            if "verbose_enabled" in disk_data.keys():
+                self.verbose_enabled = disk_data["verbose_enabled"]
+            if "notify_on_outdated_version_enabled" in disk_data.keys():
+                self.notify_on_outdated_version_enabled = disk_data["notify_on_outdated_version_enabled"]
+            if "force_moordyn_support_enabled" in disk_data.keys():
+                self.force_moordyn_support_enabled = disk_data["force_moordyn_support_enabled"]
 
     def persist(self) -> None:
         """ Persists the current settings to disk for next instantiations to load. """
-        with open(self.get_save_file(), "wb") as picklefile:
-            pickle.dump(self, picklefile, PICKLE_PROTOCOL)
+        with open(self.get_save_file(), "w") as save_file:
+            json.dump(self.__dict__, save_file, indent=4)
